413c58cafae1bdb6ae16729203d36220935ac219
AbdullahKaratas <abdullah.karatas@icloud.com>
2022-06-30 19:42:53 +0200

    Kontaktformular funktiniert und ist fertig

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,59 @@
 import React, { Component } from "react";
 import { Fade, Slide } from "react-reveal";
+// import { send } from 'emailjs-com';
+import emailjs from '@emailjs/browser';
 
 class Contact extends Component {
+
+  constructor(props) {
+    super(props);
+    this.form = React.createRef();
+  }
+
+  sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_rauforg', 'template_2fas96o', this.form.current, 'JF-5rSuh1c219hUzS')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     toSend: [{
+  //       from_name: '',
+  //       to_name: '',
+  //       message: '',
+  //       reply_to: '',
+  //     }]
+  //   };  
+  // }
+  
+  // onSubmit = (e) => {
+  //   e.preventDefault();
+  //   send(
+  //     'service_rauforg',
+  //     'template_2fas96o',
+  //     'JF-5rSuh1c219hUzS'
+  //   ).then((response) => {
+  //       console.log('SUCCESS!', response.status, response.text);
+  //     }).catch((err) => {
+  //       console.log('FAILED...', err);
+  //     });
+  //   };
+
+  // handleChange = (e) => {
+  //   this.setState({ 
+  //     ...this.state.toSend, 
+  //     [e.target.name]: 
+  //     e.target.value 
+  //   });
+  // };
+
   render() {
     if (!this.props.data) return null;
 
@@ -28,11 +80,10 @@ class Contact extends Component {
             </div>
           </div>
         </Fade>
-
         <div className="row">
           <Slide left duration={1000}>
             <div className="eight columns">
-              <form action="" method="post" id="contactForm" name="contactForm">
+              {/* <form onSubmit={this.onSubmit} id="contactForm" name="contactForm">
                 <fieldset>
                   <div>
                     <label htmlFor="contactName">
@@ -43,21 +94,36 @@ class Contact extends Component {
                       defaultValue=""
                       size="35"
                       id="contactName"
-                      name="contactName"
+                      name='from_name'
+                      value={this.state.toSend.from_name}
                       onChange={this.handleChange}
                     />
                   </div>
-
+                  <div>
+                    <label htmlFor="contactTo">
+                      An <span className="required">*</span>
+                    </label>
+                    <input
+                      type='text'
+                      defaultValue=""
+                      size="35"
+                      id="contactTo"
+                      name='to_name'
+                      value={this.state.toSend.to_name}
+                      onChange={this.handleChange}
+                    />
+                  </div>
                   <div>
                     <label htmlFor="contactEmail">
                       Email <span className="required">*</span>
                     </label>
                     <input
                       type="text"
+                      name='reply_to'
                       defaultValue=""
                       size="35"
+                      value={this.state.toSend.reply_to}
                       id="contactEmail"
-                      name="contactEmail"
                       onChange={this.handleChange}
                     />
                   </div>
@@ -82,10 +148,12 @@ class Contact extends Component {
                       cols="50"
                       rows="15"
                       id="contactMessage"
-                      name="contactMessage"
+                      type='text'
+                      name='message'
+                      value={this.state.toSend.message}
+                      onChange={this.handleChange}
                     ></textarea>
                   </div>
-
                   <div>
                     <button className="submit">Submit</button>
                     <span id="image-loader">
@@ -93,6 +161,15 @@ class Contact extends Component {
                     </span>
                   </div>
                 </fieldset>
+              </form> */}
+              <form ref={this.form} onSubmit={this.sendEmail}>
+                <label>Name</label>
+                  <input type="text" name="from_name" />
+                <label>Email</label>
+                  <input type="email" name="from_email" />
+                <label>Message</label>
+                  <textarea name="message" />
+                <input type="submit" value="Send" />
               </form>
 
               <div id="message-warning"> Error boy</div>
