57a88354f4d15abb318b9403a80894c65056892b
AbdullahKaratas <abdullah.karatas@icloud.com>
2022-07-02 09:39:03 +0200

    Toast after sending and change button for send

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,59 +1,43 @@
 import React, { Component } from "react";
 import { Fade, Slide } from "react-reveal";
-// import { send } from 'emailjs-com';
 import emailjs from '@emailjs/browser';
+import toast, { Toaster } from 'react-hot-toast';
+
 
 class Contact extends Component {
 
   constructor(props) {
     super(props);
     this.form = React.createRef();
+    this.state = {
+      open: false
+    }
   }
 
+  notify = () => toast('Die Email ist angekommen.');
+
+
   sendEmail = (e) => {
+    this.state.open = true;
     e.preventDefault();
 
     emailjs.sendForm('service_rauforg', 'template_2fas96o', this.form.current, 'JF-5rSuh1c219hUzS')
       .then((result) => {
-          console.log(result.text);
+        this.notify()
+        // <Snackbar open={this.state.open} autoHideDuration={6000} message="Email gesendet" />
       }, (error) => {
           console.log(error.text);
       });
   };
 
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     toSend: [{
-  //       from_name: '',
-  //       to_name: '',
-  //       message: '',
-  //       reply_to: '',
-  //     }]
-  //   };  
-  // }
+  handleClick = () => {
+    this.state.open = true;
+  };
+
+  handleClose = () => {
+    this.state.open = false;
+  };
   
-  // onSubmit = (e) => {
-  //   e.preventDefault();
-  //   send(
-  //     'service_rauforg',
-  //     'template_2fas96o',
-  //     'JF-5rSuh1c219hUzS'
-  //   ).then((response) => {
-  //       console.log('SUCCESS!', response.status, response.text);
-  //     }).catch((err) => {
-  //       console.log('FAILED...', err);
-  //     });
-  //   };
-
-  // handleChange = (e) => {
-  //   this.setState({ 
-  //     ...this.state.toSend, 
-  //     [e.target.name]: 
-  //     e.target.value 
-  //   });
-  // };
-
   render() {
     if (!this.props.data) return null;
 
@@ -83,85 +67,6 @@ class Contact extends Component {
         <div className="row">
           <Slide left duration={1000}>
             <div className="eight columns">
-              {/* <form onSubmit={this.onSubmit} id="contactForm" name="contactForm">
-                <fieldset>
-                  <div>
-                    <label htmlFor="contactName">
-                      Name <span className="required">*</span>
-                    </label>
-                    <input
-                      type="text"
-                      defaultValue=""
-                      size="35"
-                      id="contactName"
-                      name='from_name'
-                      value={this.state.toSend.from_name}
-                      onChange={this.handleChange}
-                    />
-                  </div>
-                  <div>
-                    <label htmlFor="contactTo">
-                      An <span className="required">*</span>
-                    </label>
-                    <input
-                      type='text'
-                      defaultValue=""
-                      size="35"
-                      id="contactTo"
-                      name='to_name'
-                      value={this.state.toSend.to_name}
-                      onChange={this.handleChange}
-                    />
-                  </div>
-                  <div>
-                    <label htmlFor="contactEmail">
-                      Email <span className="required">*</span>
-                    </label>
-                    <input
-                      type="text"
-                      name='reply_to'
-                      defaultValue=""
-                      size="35"
-                      value={this.state.toSend.reply_to}
-                      id="contactEmail"
-                      onChange={this.handleChange}
-                    />
-                  </div>
-
-                  <div>
-                    <label htmlFor="contactSubject">Betreff</label>
-                    <input
-                      type="text"
-                      defaultValue=""
-                      size="35"
-                      id="contactSubject"
-                      name="contactSubject"
-                      onChange={this.handleChange}
-                    />
-                  </div>
-
-                  <div>
-                    <label htmlFor="contactMessage">
-                      Message <span className="required">*</span>
-                    </label>
-                    <textarea
-                      cols="50"
-                      rows="15"
-                      id="contactMessage"
-                      type='text'
-                      name='message'
-                      value={this.state.toSend.message}
-                      onChange={this.handleChange}
-                    ></textarea>
-                  </div>
-                  <div>
-                    <button className="submit">Submit</button>
-                    <span id="image-loader">
-                      <img alt="" src="images/loader.gif" />
-                    </span>
-                  </div>
-                </fieldset>
-              </form> */}
               <form ref={this.form} onSubmit={this.sendEmail}>
                 <label>Name</label>
                   <input type="text" name="from_name" />
@@ -169,7 +74,23 @@ class Contact extends Component {
                   <input type="email" name="from_email" />
                 <label>Message</label>
                   <textarea name="message" />
-                <input type="submit" value="Send" />
+                <button className="submit" type="submit">Senden</button>
+                <Toaster
+                position="bottom-center"
+                reverseOrder={false}
+                gutter={8}
+                containerClassName=""
+                containerStyle={{}}
+                toastOptions={{
+                  // Define default options
+                  className: '',
+                  duration: 5000,
+                  style: {
+                    background: '#3fd63a',
+                    color: '#fff',
+                  },
+                }}
+              />
               </form>
 
               <div id="message-warning"> Error boy</div>
